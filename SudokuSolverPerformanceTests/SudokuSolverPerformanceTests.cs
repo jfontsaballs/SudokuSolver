66a81024c3e2434c9b9b399e0860d08a16980d99
Jordi Font Saballs <jfontsaballs@gmail.com>
2020-07-12 20:12:49 +0200

    Performance tests

---
+++ SudokuSolverPerformanceTests/SudokuSolverPerformanceTests.cs
@@ -0,0 +1,29 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using SudokuSolver;
+using SudokuSolver.Tests;
+using System;
+
+namespace SudokuSolverPerformanceTests
+{
+    public class SudokuSolverPerformanceTests
+    {
+        [Benchmark]
+        public Sudoku BruteForceSolver_Facil() => BruteForceSolver.Solve(Sudokus.Easy.Sudoku);
+
+        [Benchmark]
+        public Sudoku BruteForceSolver_Intermedi() => BruteForceSolver.Solve(Sudokus.Intermediate.Sudoku);
+
+        [Benchmark]
+        public Sudoku BruteForceSolver_Dificil() => BruteForceSolver.Solve(Sudokus.Difficult.Sudoku);
+       
+        [Benchmark]
+        public Sudoku BruteForceSolver_Expert() => BruteForceSolver.Solve(Sudokus.Expert.Sudoku);
+
+        static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<SudokuSolverPerformanceTests>();
+            Console.ReadLine();
+        }
+    }
+}
